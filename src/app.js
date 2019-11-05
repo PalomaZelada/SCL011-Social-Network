68b9b053b7699244628b8f56d02963aa64b35013
vero688 <vero.urrutiaa@gmila.coname>
2019-11-05 13:38:56 -0300

    agregado cambios en css

--- src/app.js
+++ src/app.js
@@ -5,3 +5,16 @@ const init = () => {
 };
  
  window.addEventListener('load', init);
+
+ var db = firebase.firestore();
+ db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
