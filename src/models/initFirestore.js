5e84030020988ba026b3a439d420de3d9d3df475
PalomaZelada <p.zelada.m@gmail.com>
2019-11-11 12:34:04 -0300

    Cambios en user.js, se agrega observador y cambio de vistas

--- src/models/initFirestore.js
+++ src/models/initFirestore.js
@@ -1,4 +1,4 @@
-export const addNewUser = ()=> { 
+export const addNewUser = () => { 
     let getName = document.getElementById("name").value;
     let getEmail = document.getElementById("newEmail").value;
     let getPassword = document.getElementById("newPassword").value;
@@ -23,7 +23,7 @@ export const addNewUser = ()=> {
 }
 
 
-  export const addNewPost = ()=> { 
+  export const addNewPost = () => { 
     let getPost = document.getElementById("newPost").value;
 
     var db = firebase.firestore();
@@ -34,22 +34,44 @@ export const addNewUser = ()=> {
   })
   .then(function(docRef) {
       console.log("Document written with ID: ", docRef.id);
+      document.getElementById("newPost").value = '';
   })
   .catch(function(error) {
       console.error("Error adding document: ", error);
   });
 
 }
-export const showAllPost = () =>{
+export const showAllPost = () => {
 var db = firebase.firestore();
 
 let showPost = document.getElementById("publishPost")
-db.collection("Posts").get().then((querySnapshot) => {
+db.collection("Posts").onSnapshot((querySnapshot) => {
     querySnapshot.forEach((doc) => {
+        
         console.log(`${doc.id} => ${doc.data().newComment}`);
         showPost.innerHTML += `
-        <div>${doc.data().newComment}</div>
+        <!--<p id="postId">${doc.id}</p>-->
+        <textarea rows="10" cols="70" readonly>${doc.data().newComment}</textarea>
+        <button id=deletePost>Borrar</button>
+        <button id=editPost>Editar</button>
         `
+    let getPostId = doc.id
+    const deleteBtn = document.getElementById("deletePost");
+    deleteBtn.addEventListener('click', () => {
+    deletePost(getPostId);
+    console.log("boton funciona")
+})
     });
-}); 
+});
+
 }
+function deletePost (id){
+    var db = firebase.firestore();
+
+    db.collection("Posts").doc(id).delete().then(function() {
+        console.log("Document successfully deleted!");
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+}
+
--- src/views/templateHome.js
+++ src/views/templateHome.js
@@ -1,11 +1,12 @@
-import { addNewPost } from "../models/initFirestore.js"
-import { showAllPost } from "../models/initFirestore.js"
+import { addNewPost, showAllPost } from "../models/initFirestore.js"
+import { logOut } from "../models/user.js"
 
 export const templateHome = () =>{
     const containerHome = document.createElement("div");
 
     const contentHome = `<h3>HOME</h3>
     <textarea id="newPost" rows="10" cols="70" placeholder="Deja tu opinion"></textarea>
+    <button id="logOut">Cerrar Sesion</button>
     <button id="publish"> Publicar </button>
     <div id="publishPost"></div>
     `
@@ -13,12 +14,16 @@ export const templateHome = () =>{
     containerHome.innerHTML = contentHome;
     document.getElementById("root").appendChild(containerHome).innerHTML;
 
+    const logOutBtn = document.getElementById("logOut");
+    logOutBtn.addEventListener('click', () => {
+        logOut();
+    });
+
     const publishBtn = document.getElementById("publish");
     publishBtn.addEventListener('click',() => {
         addNewPost();
         showAllPost();
-    })
 
-
+    });
     return containerHome;
 }
